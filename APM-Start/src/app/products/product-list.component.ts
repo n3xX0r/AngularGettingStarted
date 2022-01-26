e2bb1066ddf27097cd72671788c67808fcfc10ad
Manuel Grolitsch <m.grolitsch@tricentis.com>
2022-01-26 23:13:14 +0100

    06 Data Bindings & Pipes

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -6,6 +6,10 @@ import { Component } from "@angular/core";
 })
 export class ProductListComponent{
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
     products: any[] = [        
             {
               "productId": 1,
@@ -26,6 +30,39 @@ export class ProductListComponent{
               "price": 32.99,
               "starRating": 4.2,
               "imageUrl": "assets/images/garden_cart.png"
-            }          
+            },
+            {
+              "productId": 5,
+              "productName": "Hammer",
+              "productCode": "TBX-0048",
+              "releaseDate": "May 21, 2021",
+              "description": "Curved claw steel hammer",
+              "price": 8.9,
+              "starRating": 4.8,
+              "imageUrl": "assets/images/hammer.png"
+            },
+            {
+              "productId": 8,
+              "productName": "Saw",
+              "productCode": "TBX-0022",
+              "releaseDate": "May 15, 2021",
+              "description": "15-inch steel blade hand saw",
+              "price": 11.55,
+              "starRating": 3.7,
+              "imageUrl": "assets/images/saw.png"
+            },
+            {
+              "productId": 10,
+              "productName": "Video Game Controller",
+              "productCode": "GMG-0042",
+              "releaseDate": "October 15, 2020",
+              "description": "Standard two-button video game controller",
+              "price": 35.95,
+              "starRating": 4.6,
+              "imageUrl": "assets/images/xbox-controller.png"
+            }
     ];
+    toggleImage():void {
+        this.showImage = !this.showImage;
+    }
 }
