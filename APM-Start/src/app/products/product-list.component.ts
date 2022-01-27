d3ad89435138fee129932b79e10f378b31c48372
Manuel Grolitsch <m.grolitsch@tricentis.com>
2022-01-28 00:18:22 +0100

    07 More on Components

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,16 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
-    products: any[] = [        
+    // listFilter: string = 'cart';
+
+    private _listFilter: string = "";
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        console.log("In setter: ", value);
+        this.filteredProducts = this.performFilter(value);
+    }
+
+    filteredProducts: IProduct[] = [];
+    products: IProduct[] = [        
             {
               "productId": 1,
               "productName": "Leaf Rake",
@@ -62,7 +76,19 @@ export class ProductListComponent{
               "imageUrl": "assets/images/xbox-controller.png"
             }
     ];
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((products: IProduct) =>
+            products.productName.toLocaleLowerCase().includes(filterBy));
+    }
+
     toggleImage():void {
         this.showImage = !this.showImage;
     }
+    ngOnInit(): void {
+        this.listFilter ="cart";
+    }
+
+
 }
